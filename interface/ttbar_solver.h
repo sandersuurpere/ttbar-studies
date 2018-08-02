ada44272f2e50018f13e8f4a47d7e0ebd811a713
sndrsuur <sander.suurpere@cern.ch>
2018-08-02 13:35:59 +0200

    added chi^2

--- interface/ttbar_solver.h
+++ interface/ttbar_solver.h
@@ -3,27 +3,36 @@
 class ttbar_solver{
 	public:
 		void setLightJetA(const TLorentzVector& jet){
-		TLorentzVector ljeta=jet;
+			ljeta=jet;
 		}
 		void setLightJetB(TLorentzVector jet){
-		TLorentzVector ljetb=jet;
+			ljetb=jet;
+		}
+		void setLightJetC(TLorentzVector jet){
+			ljetc=jet;
+			fh = true;
+		}
+		void setLightJetD(TLorentzVector jet){
+			ljetc=jet;
+			fh = true;
 		}
 		void setBJetA(TLorentzVector jet){
-		TLorentzVector bjeta=jet;
+			bjeta=jet;
 		}
 		void setBJetB(TLorentzVector jet){
-		TLorentzVector bjetb=jet;
+			bjetb=jet;
 		}
 		void setLepton(TLorentzVector lep){
-		TLorentzVector lepton=lep;
+			lepton=lep;
+			fh = false;
 		}
 		void setNeutrino(TLorentzVector neu){
-		TLorentzVector neutrino=neu;
+			neutrino=neu;
 		}
 		double getChi2()const;
 
 	private:
 		TLorentzVector getNeutrinoVector(TLorentzVector lepton, TLorentzVector met);
-		TLorentzVector ljeta, ljetb, bjeta, bjetb, leptona, neutrino;
-
+		TLorentzVector ljeta, ljetb, ljetc, ljetd, bjeta, bjetb, lepton, neutrino;
+		bool fh = false;
 };
